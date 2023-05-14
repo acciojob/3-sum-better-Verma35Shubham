0d48f5320ef06f4fa531ce238295f4efa969604d
Shubham Verma <verma.shubham2537@gmail.com>
2023-05-14 18:05:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 function threeSum(arr, target) {
-  const triplets = [];
   arr.sort((a, b) => a - b);
+  let closestSum = arr[0] + arr[1] + arr[2];
   for (let i = 0; i < arr.length - 2; i++) {
-    if (i === 0 || (i > 0 && arr[i] !== arr[i - 1])) {
-      let lo = i + 1;
-      let hi = arr.length - 1;
-      let sum = target - arr[i];
-      while (lo < hi) {
-        if (arr[lo] + arr[hi] === sum) {
-          triplets.push([arr[i], arr[lo], arr[hi]]);
-          while (lo < hi && arr[lo] === arr[lo + 1]) lo++;
-          while (lo < hi && arr[hi] === arr[hi - 1]) hi--;
-          lo++;
-          hi--;
-        } else if (arr[lo] + arr[hi] < sum) {
-          lo++;
-        } else {
-          hi--;
-        }
+    let left = i + 1;
+    let right = arr.length - 1;
+    while (left < right) {
+      let sum = arr[i] + arr[left] + arr[right];
+      if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+        closestSum = sum;
+      }
+      if (sum < target) {
+        left++;
+      } else {
+        right--;
       }
     }
   }
-  return triplets;
+  return closestSum;
 }
 
-module.exports = threeSum;
+function findClosestSum() {
+  let arrStr = document.getElementById("array-input").value;
+  let target = Number(document.getElementById("target-input").value);
+  let arr = arrStr.split(",").map(Number);
+  let closestSum = threeSum(arr, target);
+  document.getElementById("result").innerHTML = "Closest sum: " + closestSum;
+}
