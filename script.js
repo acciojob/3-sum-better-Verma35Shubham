33dbce59e51b2a873fc6fbace6057b65bf15806d
Shubham Verma <verma.shubham2537@gmail.com>
2023-05-24 16:30:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ function threeSum(arr, target) {
 
 	let closest = Infinity;
 	for (let i=0; i<arr.length-2; i++) {
-		let left = i+1; right = arr.length-1;
+		let left = i+1; 
+		let right = arr.length-1;
 		while(left<right){
 			let localSum = arr[i] + arr[left] + arr[right];
 			if(Math.abs(localSum-target) < Math.abs(closest-target)){
